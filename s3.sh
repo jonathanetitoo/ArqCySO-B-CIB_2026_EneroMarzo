23d3982a734af13b0ba54408b3363a084dfc0c67
Jonathan Tito <jotitoon@uide.edu.ec>
2026-02-05 01:46:29 +0000

    wip: last changes

--- s3.sh
+++ s3.sh
@@ -3,3 +3,15 @@ echo -n "a" > letra.txt
 
 # 2. Ver su representación en bits (usando xxd para volcado binario)
 xxd -b letra.txt
+
+# Ver permisos actuales (mira la primera columna, ej: -rw-r--r--)
+ls -l letra.txt
+
+# Cambiar a permisos 755 (rwx para dueño, r-x para grupo, r-x para otros)
+chmod 755 letra.txt
+
+# Verificar cambio
+ls -l letra.txt
+
+# volcado hexadecimal del archivo letra.txt
+hexdump -C letra.txt
